13497986e0bae04063e602eea2f77d5aa3fe6e7b
minggui.li <651718290@qq.com>
2014-04-06 00:01:11 +0800

    bug fix

--- arch/tiny.sh
+++ arch/tiny.sh
@@ -1,44 +1,112 @@
 #!/bin/bash
 
-function passwd 
+##############################################################
+# title		: arch tiny installer
+# authors	: keminar
+# contact	: https://github.com/keminar/linux-tools
+# date		: 05.04.2014
+# license	: GPLv2
+# usage		: run ./tiny.sh
+##############################################################
+
+# colors
+unset ALL_OFF GREEN RED
+ALL_OFF='\e[1;0m'
+GREEN='\e[1;32m'
+RED='\e[1;31m'
+readonly ALL_OFF GREEN RED
+
+# start
+function start
 {
-    echo "Set root passwd"
-    echo root:123456 | chpasswd
+	printf "$RED"
+cat << EOF
+---------------------------------------
+		  install start
+---------------------------------------
+EOF
+	printf "$ALL_OFF"
 }
 
+# network config
+function network
+{
+	read -p "Connect internect type [wlan|wifi]: " type
+	if [ "$type" == "wifi" ];then
+		wifi-menu
+	else
+		dhcpcd
+	fi
+}
+
+# select mirror
 function mirror
 {
-    echo "Set 163.com mirror"
-    mirror=`grep .163.com /etc/pacman.d/mirrorlist |head -n1`
-    sed -i '5a\Server = http://mirrors.163.com/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
+	echo "Set 163.com mirror"
+	mirror=`grep .163.com /etc/pacman.d/mirrorlist |head -n1`
+	sed -i '5a\Server = http://mirrors.163.com/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
+	pacman -Sy
 }
 
-function netconfig
-{
-    read -p "Connect internect type [wlan|wifi]: " type
-    if [ "$type" == "wlan" ];then
-	dhcpcd
-    else
-	wifi-menu
-    fi
-}
-
+# fdisk device
 function mkdisk
 {
-    lsblk
-    read -p "Input disk [sda|hda]: " disk
-    [[ $(lsblk -dno TYPE "/dev/$disk") = 'disk' ]] || mkdisk
-    fdisk /dev/$disk
-    fdisk -l /dev/$disk
-    read -p "Do you want to format ${disk}1 device use mkfs.ext4 [y|n]? " dev
-    if [ "$dev" == "y" ]; then
-	mkfs.ext4 /dev/${disk}1
-    fi
-    mount /dev/${disk}1 /mnt
+	lsblk
+	read -p "Input disk [sda|hda]: " disk
+	[[ $(lsblk -dno TYPE "/dev/$disk") = 'disk' ]] || mkdisk
+	fdisk /dev/$disk
+	fdisk -l /dev/$disk
+	while :; do
+		read -p "Which device will be install system [${disk}1]: " dev
+		[[ $(lsblk -dno TYPE "/dev/$dev") = 'part' ]] && break
+	done
+
+	read -p "Do you want to format ${dev} device use mkfs.ext4 [y|n]: " ans
+	if [ "$ans" == "y" ]; then
+		mkfs.ext4 /dev/$dev
+	fi
+	mount /dev/$dev /mnt
+	return $dev
 }
 
+# pacstrap && fstab
+function strap
+{
+	echo "start pacstrap base system"
+	pacstrap /mnt base
+	genfstab -U -p /mnt >> /mnt/etc/fstab
+}
 
-#netconfig
-#mirror
-#passwd
-#mkdisk
+# chroot && grub
+function chroot
+{
+	echo "Set arch-chroot"
+	dev=$(mount -l|grep ' /mnt '|awk '{print $1}')
+	printf '%s\n' "mkinitcpio -p linux; \
+	pacman -S grub; \
+	grub-install --recheck $dev; \
+	grub-mkconfig -o /boot/grub/grub.cfg
+	" |arch-chroot /mnt
+}
+
+# success
+function success
+{
+	umount /mnt
+	printf "$RED"
+cat << EOF
+---------------------------------------
+		Installation completed!
+	 Reboot the computer: # reboot
+---------------------------------------
+EOF
+	printf "$ALL_OFF"
+}
+
+start
+network
+mirror
+mkdisk
+strap
+chroot
+success
