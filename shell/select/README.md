c6f103e42b93ad1957fcca5ee7ba27b5c479a9ab
keminar <linuxphp@126.com>
2022-09-24 18:07:25 +0800

    add clear

--- shell/select/README.md
+++ shell/select/README.md
@@ -1,21 +1,37 @@
-# 选择执行
+# 选择执行, 功能如下
 
 * DB连接
 * SSH连接
 
 # 配置
 
-默认配置在~/.select/目录
+默认配置在~/.select/目录, 按shell文件名命名
+
+# 使用
+```
+cp select.sh ssh.sh
+bash ssh.sh
+
+cp select.sh db.sh
+bash db.sh
+```
 
 # 记录ssh密码
 
-在配置文件中增加#rsa=开头的配置如
+在配置文件中增加#rsa=开头的配置,可以配置多个
+
+如
 ```
 #rsa=/root/.ssh/id_rsa
+#rsa=/root/.ssh/id_rsa2
+ssh1=ssh root@127.0.0.1
+ssh2=ssh root@127.0.0.1
 ```
-可以配置多个
 
-# 关闭密码
+# 关闭全部代理
 ```
 ./select.sh close
 ```
+
+# 删除当前代理的密钥
+./select.sh clear
--- shell/select/select.sh
+++ shell/select/select.sh
@@ -5,6 +5,7 @@ SCRIPT=$0
 ACTION=$1
 # 配置默认目录
 confDir=~/.select
+agentFile=$confDir/$(basename $SCRIPT .sh).agent
 
 # 当前时间
 timeNow=`date +%s`
@@ -14,61 +15,75 @@ timeEnd=`date +%s -d '21:00:00'`
 timeExpire=$(($timeEnd - $timeNow))
 # 如果不足1小时,按1小时有效
 if [ $timeExpire -lt 3600 ];then
-   timeExpire=3600
+    timeExpire=3600
 fi
 
 # 关闭记录的密码
 function agent_close
 {
-   if [ "$ACTION" = "close" ];then
-      echo "killall ssh-agent"
-      killall ssh-agent
-      exit 0
-   fi
+    if [ "$ACTION" = "close" ];then
+        # 关闭所有代理
+        echo "killall ssh-agent"
+        if pgrep ssh-agent >/dev/null;then
+            killall ssh-agent
+        fi
+        exit 0
+    elif [ "$ACTION" = "clear" ];then
+        # 清理当前代理密钥
+        if [ -f $agentFile ];then
+            source $agentFile >/dev/null
+            if ps -p $SSH_AGENT_PID > /dev/null; then
+                # 清理密钥
+                ssh-add -D
+                exit 0
+            fi
+        fi
+        echo "ssh-agent process not found"
+        exit 0
+    fi
 }
 
 # 检查配置
 function check_config
 {
-   if [ ! -d $confDir ];then
-     mkdir $confDir
-   fi
-   confFile=$confDir/$(basename $SCRIPT .sh).conf
-   if [ ! -f $confFile ];then
-     echo "$confFile not found"
-     exit 1
-   fi
+    if [ ! -d $confDir ];then
+        mkdir $confDir
+    fi
+    confFile=$confDir/$(basename $SCRIPT .sh).conf
+    if [ ! -f $confFile ];then
+        echo "$confFile not found"
+        exit 1
+    fi
 }
 
 # 检查是否要记录ssh密码
 function agent_add
 {
-   rsaList=`grep '^#rsa=' $confFile|awk -F '=' '{for(i=2;i<NF;i++)printf("%s=",$i);print $NF}'`
-   if [ "$rsaList" != "" ];then
-     agentFile=$confDir/$(basename $SCRIPT .sh).agent
-     if [ ! -f $agentFile ];then
-       echo "start ssh-agent"
-       ssh-agent -s >$agentFile 2>/dev/null
-     fi
-     source $agentFile >/dev/null
-     # 如果不存在表示过期,要重新生成
-     if ! ps -p $SSH_AGENT_PID > /dev/null; then
-     #if [ ! -e $SSH_AUTH_SOCK ];then
-       echo "start ssh-agent"
-       ssh-agent -s >$agentFile 2>/dev/null
-       source $agentFile >/dev/null
-     fi
-     # 临时把错误忽略, 不然ssh-add出错时会退出
-     set +e
-     for rsa in $rsaList
-     do
-       ssh-add -l 2>/dev/null|grep $rsa >/dev/null
-       if [ "$?" != "0" ] ;then
-         ssh-add -t $timeExpire $rsa
+    rsaList=`grep '^#rsa=' $confFile|awk -F '=' '{for(i=2;i<NF;i++)printf("%s=",$i);print $NF}'`
+    if [ "$rsaList" != "" ];then
+       if [ ! -f $agentFile ];then
+           echo "start ssh-agent"
+           ssh-agent -s >$agentFile 2>/dev/null
        fi
-     done
-     set -e
-   fi
+       source $agentFile >/dev/null
+       # 如果不存在表示过期,要重新生成
+       if ! ps -p $SSH_AGENT_PID > /dev/null; then
+       #if [ ! -e $SSH_AUTH_SOCK ];then
+           echo "start ssh-agent"
+           ssh-agent -s >$agentFile 2>/dev/null
+           source $agentFile >/dev/null
+       fi
+       # 临时把错误忽略, 不然ssh-add出错时会退出
+       set +e
+       for rsa in $rsaList
+       do
+           ssh-add -l 2>/dev/null|grep $rsa >/dev/null
+           if [ "$?" != "0" ] ;then
+               ssh-add -t $timeExpire $rsa
+           fi
+       done
+       set -e
+    fi
 }
 
 function select_cmd
@@ -103,13 +118,13 @@ function select_cmd
 
 # 不可用source调用
 if [ "$SCRIPT" = "bash" -o "$SCRIPT" = "-bash" -o "${SCRIPT%/bin/bash}" != "${SCRIPT}" ]; then
-   echo "Do not use source command"
-   # 把错误回滚,不然后面再操作shell出错时会退掉终端
-   set +e
+    echo "Do not use source command"
+    # 把错误回滚,不然后面再操作shell出错时会退掉终端
+    set +e
 else
-   agent_close
-   check_config
-   agent_add
-   select_cmd
+    agent_close
+    check_config
+    agent_add
+    select_cmd
 fi
 
