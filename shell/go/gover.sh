c29f9c9a05988322fdf86ba3344577be1681dde4
keminar <linuxphp@126.com>
2023-03-06 15:21:21 +0800

    version

--- shell/go/gover.sh
+++ shell/go/gover.sh
@@ -8,15 +8,25 @@ if [ "$dir" = "" ]; then
    exit 2
 fi
 
-lists=`find $dir -depth -maxdepth 1 -mindepth 1 -type d -name "go*"|awk -F '/' '{print $NF}'`
+if [ ! -L "$dir/go" ];then
+    echo "The $dir/go is not a symbolic link"
+    exit 3
+fi
+link=`ls -al $dir/go|awk '{print $NF}'`
+oldVer=`basename $link`
+read -p "Current version $oldVer, do you need to change [y|n]?" answer
+if [ "$answer" = 'n' ];then
+    exit
+fi
 
 ver=""
+lists=`find $dir -depth -maxdepth 1 -mindepth 1 -type d -name "go*"|awk -F '/' '{print $NF}'`
 select opt in $lists
 do
     if [ "$opt" = "" ]; then
         echo "Invalid input, please retry"
     else
-        echo "selected: $opt"
+        #echo "selected: $opt"
         for line in $lists
         do
             if [ "$line" = "$opt" ]; then
@@ -27,23 +37,14 @@ do
     fi
 done
 
-if [ ! -L "$dir/go" ];then
-    echo "The $dir/go is not a symbolic link"
-    exit 3
-fi
-link=`ls -al $dir/go|awk '{print $NF}'`
-oldVer=`basename $link`
-
 if [ "$oldVer" = "$ver" ];then
     echo "The new version is the same as the old version"
     exit
 fi
-echo "go version from $oldVer => $ver"
-fullVer=`echo "$dir/$ver"|sed 's#\/\/#\/#g'`
 
+fullVer=`echo "$dir/$ver"|sed 's#\/\/#\/#g'`
 rm -f $dir/go
 ln -sf $fullVer $dir/go
-
 link=`ls -al $dir/go|awk '{print $NF}'`
 newVer=`basename $link`
 echo "New version $newVer"
